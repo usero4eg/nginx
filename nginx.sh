6b1cabef8ee7fab6305967c8a9a3f397fa46ea83
Dmitry Sedyakin <42033012+usero4eg@users.noreply.github.com>
2020-04-07 14:55:56 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -4,9 +4,11 @@ mkdir ~/Downloads
 cd ~/Downloads
 wget https://github.com/usero4eg/nginx/nginx.repo
 mv nginx.repo /etc/yum.repos.d/
+yum install epel-release
 yum install -y yum-utils
-yum-config-manager --enable nginx-mainline
 yum update
+yum upgrade
+yum-config-manager --enable nginx-mainline
 yum install -y nginx
 systemctl enable nginx
 systemctl start nginx
