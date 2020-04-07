2a2091310d25158a255990d0d9036246dc194b90
Dmitry Sedyakin <42033012+usero4eg@users.noreply.github.com>
2020-04-07 12:02:53 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -11,6 +11,6 @@ tar -xzvf nginx-1.14.2.tar.gz
 cd /home/sandbox/downloads/nginx/nginx-1.14.2
 
 # Assembling package with requirement
-./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access/log --with-pcre --with-http_ssl_module
+./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access.log --with-pcre --with-http_ssl_module
 make
 make install
