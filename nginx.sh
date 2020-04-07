8b7c845484ef0a5464a91aeb8f75faa5d12ab4a2
Dmitry Sedyakin <42033012+usero4eg@users.noreply.github.com>
2020-04-07 15:27:43 +0300

    added certbot for nginx repo

--- nginx.sh
+++ nginx.sh
@@ -1,10 +1,10 @@
 #! /bin/bash
 
 mv ~/Downloads/nginx/nginx.repo /etc/yum.repos.d/
-yum install -y epel-release yum-utils elinks
+yum install -y epel-release yum-utils net-tools elinks
 yum update
 yum upgrade
-yum-config-manager --enable nginx-mainline
+yum-config-manager --enable nginx-mainline rhui-REGION-rhel-server-extras rhui-REGION-rhel-server-optional
 yum install -y nginx
 systemctl enable nginx
 systemctl start nginx
