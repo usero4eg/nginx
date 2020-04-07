31741149e3b37f50a204ff837e6ff9adf61517d5
Dmitry Sedyakin <42033012+usero4eg@users.noreply.github.com>
2020-04-07 11:49:15 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -8,10 +8,9 @@ yum -y install pcre-devel openssl-devel
 cd /home/sandbox/downloads
 wget https://nginx.org/download/nginx-1.14.2.tar.gz
 tar -xzvf nginx-1.14.2.tar.gz
-cd nginx-1.13.2
+cd /home/sandbox/downloads/nginx/nginx-1.14.2
 
 # Assembling package with requirement
-cd /home/sandbox/downloads/nginx/nginx-1.14.2
 ./configure --sbin-path=/usr/bin/nginx --conf-path=/etc/nginx/nginx.conf --error-log-path=/var/log/nginx/error.log --http-log-path=/var/log/nginx/access/log --with-pcre --with-http_ssl_module
 make
 make install
