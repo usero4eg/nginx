02d1676357abd02c1b83f0946b73b45d084c30bf
Dmitry Sedyakin <42033012+usero4eg@users.noreply.github.com>
2020-04-07 15:14:06 +0300

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 mv ~/Downloads/nginx/nginx.repo /etc/yum.repos.d/
-yum install -y yum-utils epel-release
+yum install -y epel-release yum-utils elinks
 yum update
 yum upgrade
 yum-config-manager --enable nginx-mainline
-yum install -y elink nginx
+yum install -y nginx
 systemctl enable nginx
 systemctl start nginx
-elink http://localhost
+elinks http://localhost
